39984fa27497d1b836679df106ce1690474f6525
Mayra Rabelo <mayra.silva.rabelo@gmail.com>
2022-10-24 20:40:55 -0300

    Estilo página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import db from './services/db.json';
+import { useState } from 'react';
 
 function App() {
+
+  const [searchName, setSearchName] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="flex">
+        <h1>MySocial</h1>
+        <input 
+          type="search" 
+          id="site-search" 
+          placeholder='search' 
+          onChange={(event) => {
+            setSearchName(event.target.value)
+          }}>
+        </input>
       </header>
+      <main className='flex flex-wrap-wrap'>
+        {
+          db.filter((val) => {
+            if( searchName == '') {
+              return val
+            } else if (val.name.toLowerCase().includes(searchName.toLowerCase())) {
+              return val
+            }
+          }).map((val, key) => {
+            return (
+              <div className='card' key={key}>
+                <div className='flex image justify-content-center'>
+                  <img src={val.picture} />
+                </div>
+                <p>name: {val.name}</p> 
+                <p>age: {val.age}</p>
+                <p>eyeColor: {val.eyeColor}</p>
+                <p>company: {val.company}</p>
+                <p>email: {val.email}</p>
+              </div>)
+          })
+        }
+      </main>
     </div>
   );
 }
